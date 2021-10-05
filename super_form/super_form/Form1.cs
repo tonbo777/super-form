f1079e1c6b410aa0cf34ebb45a0082790ae4830b
tonbo777 <jcnonakani@gmail.com>
2021-10-05 08:51:15 +0800

    initial test vs project
    test visual studio project !

---
+++ super_form/super_form/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace super_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 110;
+
+            MessageBox.Show("My MSGBOX YH");
+        }
+    }
+}
